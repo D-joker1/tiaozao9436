51cc1513f336cb9cb11af70ff254a8b31cb73221
maomao1026 <sayaka1026@163.com>
2017-08-03 13:54:07 +0800

    2017.8.3

---
+++ tzxmpp.h
@@ -0,0 +1,15 @@
+//
+//  tzxmpp.h
+//  tiaozao
+//
+//  Created by 耿直的佳哥从不胡说八道 on 2017/8/3.
+//  Copyright © 2017年 david. All rights reserved.
+//
+
+#ifndef tzxmpp_h
+#define tzxmpp_h
+
+
+#endif /* tzxmpp_h */
+
+#import "xmpp.h"
